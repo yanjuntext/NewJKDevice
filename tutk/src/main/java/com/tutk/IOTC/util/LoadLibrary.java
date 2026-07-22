6a21f0e22b3609d6138f6c8ca314a47e0cb8ebbd
yanjunw <30396035+yanjuntext@users.noreply.github.com>
2026-07-22 18:23:12 +0800

    增加T库
    增加T库

--- tutk/src/main/java/com/tutk/IOTC/util/LoadLibrary.java
+++ tutk/src/main/java/com/tutk/IOTC/util/LoadLibrary.java
@@ -1,10 +1,13 @@
 package com.tutk.IOTC.util;
 
+import android.util.Log;
+
 public class LoadLibrary {
-    public static final boolean TRACE = false;
+    public static final boolean TRACE = true;
     public static final String TAG = "LoadLibrary";
 
     public static void load(String library) {
+        Log.d(TAG, "load: " + library + "---TRACE=" + TRACE);
         System.loadLibrary(TRACE ? library + "T" : library);
     }
 }
